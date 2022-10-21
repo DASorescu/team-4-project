916d4b32f3552d69412412a52dca765bef3d942a
Soficius <103771506+Soficius@users.noreply.github.com>
2022-10-21 18:31:53 +0200

    create reviews index and shows

---
+++ resources/views/admin/users/reviews/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <header>
+        <h1 class="text-center">Reviews</h1>
+    </header>
+    <div class="container">
+        <table class="table container-fluid">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Autore</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Rating</th>
+                    <th scope="col">Content</th>
+                    <th scope="col">Azioni</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse($reviews as $review)
+                    <tr>
+                        <th scope="row">{{ $review->id }}</th>
+                        <td>{{ $review->guest_name }}</td>
+                        <td>{{ $review->guest_email }}</td>
+                        <td>{{ $review->rating }}</td>
+                        <td>{{ substr($review->content, 0, 20) }}...</td>
+                        <td><a href="{{ route('admin.users.reviews.show', $review->id) }}"
+                                class="btn btn-primary mr-4">Vedi</a>
+                        </td>
+                    </tr>
+
+                @empty
+                    <tr>
+                        <td scope="row">Nessuna Recensione</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,19 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::middleware('auth')->prefix('admin')->name('admin.')->namespace('Admin')->group( function(){
+Route::middleware('auth')->prefix('admin')->name('admin.')->namespace('Admin')->group(function () {
     Route::get('/', 'HomeController@index')->name('home');
     Route::get('/users/edit', 'Users\UserDetailController@edit')->name('users.edit');
-    Route::resource('/users/services','Users\ServiceController');
+    Route::resource('/users/services', 'Users\ServiceController');
     Route::put('/users/update', 'Users\UserDetailController@update')->name('users.update');
-    
-    Route::get('/{any}', function(){
+    // Route::resource('/users/reviews', 'Users\ReviewController');
+
+    Route::get('/users/reviews', 'Users\ReviewController@index')->name('users.reviews.index');
+    Route::get('/users/reviews/{id}', 'Users\ReviewController@show')->name('users.reviews.show');
+
+    Route::get('/{any}', function () {
         abort('404');
     })->where('any', '.*');
-        
-    
 });
 
 Route::get('/', function () {
