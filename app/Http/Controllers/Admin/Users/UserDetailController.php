7de88223a579205535dcd6b34d18567510335277
Soficius <103771506+Soficius@users.noreply.github.com>
2022-10-23 00:02:46 +0200

    fixed layout userdetail

--- app/Http/Controllers/Admin/Users/UserDetailController.php
+++ app/Http/Controllers/Admin/Users/UserDetailController.php
@@ -13,21 +13,24 @@ use App\Models\UserDetail;
 
 class UserDetailController extends Controller
 {
-    public function create(){
+    public function create()
+    {
 
         $user = Auth::user();
         return view('admin.users.create', compact('user'));
     }
 
-    public function edit(){
+    public function edit()
+    {
 
         // passo user anziché solo i dettagli in caso di form più complessi in futuro
         $user = Auth::user();
-        
+
         return view('admin.users.edit', compact('user'));
     }
 
-    public function update(Request $request){
+    public function update(Request $request)
+    {
 
 
         // anche qui separo lo user dai dettagli aiutandomi con una seconda variabile, per leggibilità
@@ -47,9 +50,9 @@ class UserDetailController extends Controller
 
                 'image' => 'nullable|mimes:jpg,jpeg,png|file',
 
-                'phone' => ['nullable','regex:^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$^'],
+                'phone' => ['nullable', 'regex:^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$^'],
 
-                'city' => ['nullable','string', "in_array:cities"]
+                'city' => ['nullable', 'string', "in_array:cities"]
             ],
             [
                 'city.in_array' => ' Pippo ',
@@ -62,35 +65,34 @@ class UserDetailController extends Controller
 
         $data = $request->all();
 
+
         // qui uso $detail per le operazioni, come detto sopra
-        if(array_key_exists('cv', $data))
-        {
-            if($detail->cv) Storage::delete($detail->cv);
+        if (array_key_exists('cv', $data)) {
+            if ($detail->cv) Storage::delete($detail->cv);
             $cv_url = Storage::put('user_cvs', $data['cv']);
             $detail->cv = $cv_url;
         }
 
-        if(array_key_exists('image', $data))
-        {
-            if($detail->image) Storage::delete($detail->image);
+        if (array_key_exists('image', $data)) {
+            if ($detail->image) Storage::delete($detail->image);
             $image_url = Storage::put('user_images', $data['image']);
             $detail->image = $image_url;
         };
 
         // aggiungo il phone (che non è più in fillable) in caso mi dovessero passare la chiave del campo phone
-        if(array_key_exists('phone', $data))
-        {
+        if (array_key_exists('phone', $data)) {
             $detail->phone = $data['phone'];
         }
-            
+
         $detail->update($data);
 
 
         // alla fine passo comunque lo user intero per eventuali paginazioni complesse del profilo (reviews ecc)
-        return redirect()->route('admin.users.edit', compact('user'))->with('message','dati modificati con successo' );
+        return redirect()->route('admin.users.edit')->with('message', 'dati modificati con successo');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
 
         $user = Auth::user();
         $cities = config('cities');
@@ -110,7 +112,7 @@ class UserDetailController extends Controller
         );
 
         $data = $request->all();
-        
+
         $new_userDetail = new UserDetail();
 
         $new_userDetail->user_id = $user->id;
@@ -121,8 +123,7 @@ class UserDetailController extends Controller
 
         // ! MAIN FIX - non avevamo salvato il nuovo oggetto UserDetail
         $new_userDetail->save();
-        
-        return redirect()->route('admin.users.edit', compact('user'))->with('message','dati inseriti con successo' );
 
+        return redirect()->route('admin.users.edit')->with('message', 'dati inseriti con successo');
     }
 }
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,140 +1,119 @@
 @extends('layouts.app')
 
+@php
+    // se non viene selezionata nessun elemento dallo storage ritorno quello alternativo
+    function defaultPathStorage($path, $alternativePath)
+    {
+        $path = asset($path);
+        $alternativePath = !empty($alternativePath) ? asset($alternativePath) : $alternativePath;
+        return Str::endsWith($path, 'storage') ? $alternativePath : $path;
+    }
+    $imagePath = defaultPathStorage('storage/' . $user->userDetail->image, 'img/default-user-image.png');
+    $curriculumPath = defaultPathStorage('storage/' . $user->userDetail->cv, '');
+
+@endphp
+
 @section('content')
-@if($user->userDetail)
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                    <div>Nome</div>
-                    <div>Cognome</div>
-                    <div>Specializzazione</div>
+    @if ($user->userDetail)
+        <div class="container">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
                 </div>
+            @endif
+            <div class="row justify-content-center mt-5">
+                <div class="col-md-6">
+                    <form action="{{ route('admin.users.update') }}" enctype="multipart/form-data" method="POST">
+                        @method('PUT')
+                        @csrf
+                        <div class="card shadow">
+                            {{-- rimosso il passaggio di $user perché lo prendiamo tramite autorizzazione --}}
+                            <div class="card-header d-flex justify-content-center">
+                                <div class="form-group" style="width: 100px; height:100px" id="profileImg"
+                                    onclick="document.getElementById('image').click()">
+                                    <input type="file" id="image" name="image" class="d-none">
+                                    <img class="img-cover border rounded-circle bg-white shadow" src="{{ $imagePath }}"
+                                        alt="user image">
+                                    <div class="overlay"></div>
+                                    <i class="fa-solid fa-pen-to-square fa-2xl"></i>
+                                </div>
+                            </div>
 
-                <div class="card-body">
-      
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="first_name">Nome</label>
+                                    <input type="text" class="form-control" id="first_name" name="first_name"
+                                        @if ($user->userDetail->first_name) value="{{ old('first_name', $user->userDetail->first_name) }}" @endif
+                                        required minlength="3">
+                                </div>
+                                <div class="form-group">
+                                    <label for="last_name">Cognome</label>
+                                    <input type="text" class="form-control" id="last_name" name="last_name"
+                                        @if ($user->userDetail->last_name) value="{{ old('last_name', $user->userDetail->last_name) }}" @endif
+                                        required minlength="3">
+                                </div>
+                                <div class="form-group">
+                                    <label for="adress">Città</label>
+                                    <select name="address" id="address" class="form-control">
+                                        <option value="">Scegli la Città</option>
+                                        @foreach (config('cities') as $city)
+                                            <option value="{{ $city }}"
+                                                {{ old('address', $user->userDetail->address) == $city ? 'selected' : '' }}>
+
+                                                {{ $city }}
+
+
+                                            </option>
+                                        @endforeach
+
+
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="phone">Numero di telefono</label>
+                                    <input type="phone" class="form-control" id="phone" name="phone"
+                                        @if ($user->userDetail->phone) value="{{ old('phone', $user->userDetail->phone) }}" @endif
+                                        required minlength="3">
+                                </div>
+                                <div class="form-group">
+                                    <label for="cv">Curriculum</label>
+                                    <div class="custom-file">
+                                        <label class="custom-file-label" for="cv">Curriculum</label>
+                                        <input type="file" name="cv" class="custom-file-input" id="cv">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="card-footer d-flex justify-content-center">
+                                <div>
+                                    <button class="btn btn-success" type="submit">
+                                        <i class="fa-solid fa-floppy-disk mr-2"></i>Salva Modifiche
+                                    </button>
+                                </div>
+                                <div class="ml-2">
+                                    <a class="btn btn-primary" href="{{ route('admin.users.sponsorships.show') }}">
+                                        <i class="fa-regular fa-file mr-2"></i>Dettagli
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-
-                <div class="card-footer">
-        
-                </div>
+                @if (!empty($curriculumPath))
+                    <div class="col-md-6">
+                        <div class="card shadow h-100">
+                            <iframe class="h-100 w-100 border border-0 rounded" src="{{ $curriculumPath }}"></iframe>
+                        </div>
+                    </div>
+                @endif
             </div>
-        </div>
-    </div>
-
-@if ($errors->any())
- <div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{$error}}</li>
-        @endforeach
-    </ul>
- </div>
-@endif
-
-{{-- rimosso il passaggio di $user perché lo prendiamo tramite autorizzazione --}}
-<form class="mt-5" action="{{ route('admin.users.update')}}" enctype="multipart/form-data" method="POST">
-    @method('PUT')
-    @csrf
-    <div class="row">
-
-       <div class="col-6">
-           <div class="form-group">
-               <label for="first_name">Nome</label>
-               <input type="text" class="form-control" id="first_name" name="first_name" @if($user->userDetail->first_name) value="{{ old('first_name', $user->userDetail->first_name) }}" @endif required minlength="3">
-           </div>
-       </div>
-
-       <div class="col-6">
-            <div class="form-group">
-                <label for="last_name">Cognome</label>
-                <input type="text" class="form-control" id="last_name" name="last_name" @if($user->userDetail->last_name) value="{{ old('last_name', $user->userDetail->last_name) }}" @endif required minlength="3">
+        @else()
+            <div class="h-100 d-flex justify-content-center">
+                <h2 class="text-danger">Devi completare il profilo Dr. Strunz!</h2>
             </div>
-       </div>
-
-       <div class="col-6">
-            <div class="form-group">
-                <select name="address" id="address">
-                    <option value="">Scegli la città</option>
-                    @foreach(config('cities') as $city)
-                        
-                        <option value="{{ $city }}" {{ old('address', $user->userDetail->address) == $city ? 'selected' : '' }}>
-                      
-                            {{$city}}
-                            
-
-                        </option>
-
-                    @endforeach
-    
-    
-                </select>
-            </div>
-        </div>
-
-        <div class="col-6">
-            <div class="form-group">
-                <label for="phone">Numero di telefono</label>
-                <input type="phone" class="form-control" id="phone" name="phone" @if($user->userDetail->phone) value="{{ old('phone', $user->userDetail->phone) }}" @endif required minlength="3">
-            </div>
-       </div>
-
-       <div class="col-12">
-
-           <div class="form-group">
-           
-                <label for="cv">Curriculum</label>
-                <input type="file" id="cv" name="cv">
-                <iframe src="{{ asset('storage/' . $user->userDetail->cv) }}" width=”100%” height=”100%”>
-            </div>
-       </div>
-
-       <div class="col-12">
-
-           <div class="form-group">
-           
-                <label for="image">Immagine</label>
-                <input type="file" id="image" name="image">
-                <img class="img-fluid" src="{{ asset('storage/' . $user->userDetail->image) }}" alt="user image preview" width="100px">
-
-            </div>
-        </div>
-    </div>
-    <footer class="d-flex align-items-center justify-content-end">
-        <div>
-            <button class="btn btn-success" type="submit">
-                <i class="fa-solid fa-floppy-disk mr-2"></i>Salva Modifiche
-            </button>
-        </div>
-        
-        <div class="ml-2">
-            <a class="btn btn-primary" href="{{route('admin.users.sponsorships.show')}}"> 
-                <i class="fa-regular fa-file mr-2"></i>Dettagli
-            </a>
-        </div>
-    </footer>
-</form>
-
-@else()
-<div class="h-100 d-flex justify-content-center">
-    <h2 class="text-danger">Devi completare il profilo Dr. Strunz!</h2>
-</div>
-@endif()
+    @endif()
 @endsection
-       
-
-
-       
-       
-            
-            
-
-       
-       
-       
-       
-       
-
-    
