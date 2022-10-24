2a8f29d598311a472342e8e65a04f33105432fff
Soficius <103771506+Soficius@users.noreply.github.com>
2022-10-24 14:08:18 +0200

    fixed width review card

--- resources/views/admin/users/reviews/show.blade.php
+++ resources/views/admin/users/reviews/show.blade.php
@@ -2,11 +2,18 @@
 
 @section('content')
     <section class="d-flex justify-content-center">
-        <div class="card d-flex" style="width: 18rem;">
+        <div class="card d-flex" style="width: 40rem;">
             <div class="card-body justify-content-center">
                 <h5 class="card-title">{{ $review->guest_name }}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">{{ $review->guest_email }}</h6>
-                <p class="card-text">{{ $review->rating }}</p>
+                <p>
+                    @for ($i = 0; $i < $review->rating; $i++)
+                        <i class="fa-solid fa-star text-warning"></i>
+                    @endfor
+                    @for ($i = 0; $i < 5 - $review->rating; $i++)
+                        <i class="fa-regular fa-star"></i>
+                    @endfor
+                </p>
                 <p class="card-text">{{ $review->content }}</p>
                 <a href="{{ route('admin.users.reviews.index', $review->id) }}" class="btn btn-primary mr-4">Torna
                     indietro</a>
