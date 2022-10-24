40a856d0598530c2a9db4b85d5a167e74080acd8
Gabri1391 <chiarellogabriele@gmail.com>
2022-10-24 16:56:39 +0200

    icon menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,13 +83,15 @@
                                     </a>
 
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
+                                        <a class="dropdown-item d-flex justify-content-between" href="{{ route('admin.users.edit') }}">
+                                            Profile <i class="fa-solid fa-user"></i>
                                         </a>
-                                        <a class="dropdown-item" href="{{ route('admin.users.edit') }}">
-                                            Profile
+
+                                        <a class="dropdown-item d-flex justify-content-between" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }} 
+                                        <i class="fa-solid fa-right-from-bracket"></i>
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST"
