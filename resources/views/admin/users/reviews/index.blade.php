310d9bdc85de863c1b31cb42f797fe259edd7603
Soficius <103771506+Soficius@users.noreply.github.com>
2022-10-24 12:32:26 +0200

    fixed stars review

--- resources/views/admin/users/reviews/index.blade.php
+++ resources/views/admin/users/reviews/index.blade.php
@@ -6,7 +6,7 @@
     </header>
     <div class="container">
         <table class="table container-fluid">
-            <thead>
+            <thead class="text-center">
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Autore</th>
@@ -16,7 +16,7 @@
                     <th scope="col">Azioni</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody class="text-center">
                 @forelse($reviews as $review)
                     <tr>
                         <th scope="row">{{ $review->id }}</th>
@@ -26,13 +26,13 @@
                             @for ($i = 0; $i < $review->rating; $i++)
                                 <i class="fa-solid fa-star text-warning"></i>
                             @endfor
-                            @for ($i = 0; $i < 10 - $review->rating; $i++)
+                            @for ($i = 0; $i < 5 - $review->rating; $i++)
                                 <i class="fa-regular fa-star"></i>
                             @endfor
                         </td>
                         <td>{{ substr($review->content, 0, 20) }}...</td>
                         <td><a href="{{ route('admin.users.reviews.show', $review->id) }}"
-                                class="btn btn-primary mr-4">Vedi</a>
+                                class="btn btn-primary text-center m-0">Vedi</a>
                         </td>
                     </tr>
 
