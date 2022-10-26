9f1d37173f8ed7839a34b06a73aed4b06b3bb387
Gabri1391 <chiarellogabriele@gmail.com>
2022-10-26 16:11:44 +0200

    layout page

---
+++ resources/js/components/pages/DoctorDetails.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div id="doctor-details">
+            Dettagli Dottore
+            <p>Città: {{ user.name}}</p>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+ export default {
+    name: "DoctorDetails",
+    data (){
+        return{
+            details:null,
+        }
+    },
+    methods:{
+        fetchDetails(){
+            axios.get('http://localhost:8000/api/users/1')
+            .then((res)=>{
+                this.details = res.data;
+            })
+            .catch((err)=>{
+                console.error(err);
+            })
+            .then(()=>{
+                console.log('chiamata terminata');
+            })
+        }
+    },
+    mounted(){
+        this.fetchDetails();
+    }
+ };
+</script>
