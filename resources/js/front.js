32eb8bb9b0bdf345a5d5c135660c098682d67942
Gabri1391 <chiarellogabriele@gmail.com>
2022-10-27 18:51:04 +0200

    import fontawesone

--- resources/js/front.js
+++ resources/js/front.js
@@ -2,6 +2,24 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faHouse)
+library.add(faPhone)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
 import App from './components/App.vue';
 
 import router from './router';
