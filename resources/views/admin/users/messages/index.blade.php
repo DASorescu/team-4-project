303f1fef20bbc6a8a6d161bc937689b2e30c1132
Gabri1391 <chiarellogabriele@gmail.com>
2022-11-03 17:02:47 +0100

    refactoring message.index

--- resources/views/admin/users/messages/index.blade.php
+++ resources/views/admin/users/messages/index.blade.php
@@ -10,45 +10,31 @@
     </div>
     @endif
 
-    
-    <h3>Lista Messaggi</h3>
-
-    
+    <h3 class="p-3">I miei messaggi</h3>
 
     <table class="table">
         <thead class="thead-dark">
             <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nome</th>
-            <th scope="col">Email</th>
-            <th scope="col">Messaggio</th>
-            <th scope="col">Mandato il</th>
-            <th scope="col">Azioni</th>
+            <th scope="col text-center">Inviato da</th>
+            <th scope="col text-center">Email</th>
+            <th scope="col ">Testo</th>
+            <th scope="col text-center">Inviato</th>
+            <th scope="col"></th>
             </tr>
         </thead>
         <tbody>
         
-            @forelse($messages as $message)
+            @forelse($messages as $message) 
 
             <tr>
-            <th scope="row">{{$message->id}}</th>
-            <td>{{$message->name}}</td>
-            <td>{{$message->email}}</td>
-            <td>{{$message->content}}</td>
-            <td>{{$message->date}}</td>
-            <td class="d-flex">
-                <a class="btn btn-primary btn-small d-flex align-items-center" href="{{ route('admin.users.messages.show', $message) }}">
+            <td class="col-2">{{$message->name}}</td>
+            <td class="col-2">{{$message->email}}</td>
+            <td class="col-4">{{ substr($message->content, 0, 50) }}...</td>
+            <td class="col-2">{{$user->created_at->format('d M h:m:s')}}</td>
+            <td class="d-flex col-1">
+                <a class="btn btn-info btn-small d-flex align-items-center" href="{{ route('admin.users.messages.show', $message) }}">
                     <i class="fa-solid fa-magnifying-glass mr-2"></i> Dettagli
                 </a>
-
-                <form action="{{ route('admin.users.messages.destroy', $message->id) }}"
-                method="POST">
-                @csrf
-                @method('DELETE')
-                <button class="btn btn-danger btn-small ml-2 d-flex align-items-center" type="submit">
-                    <i class="fa-solid fa-trash mr-2"></i> Elimina
-                </button>
-            </form>
             </td>
             </tr>
 
